a37dc13b6c21ae096fb86502bfc52ed6263bcc03
slatamer <sila.ttamer@gmail.com>
2026-05-21 15:51:07 +0300

    Kod okunabilirliği artırıldı ve açıklama satırları eklendi.

--- NetworkLog.java
+++ NetworkLog.java
@@ -1,6 +1,11 @@
+/*
+ * Network (Ağ trafik) loglarını temsil eden ve Log sınıfından türeyen alt sınıftır.
+ * Giriş/çıkış yapan veri miktarını ve oranlarını analiz ederek veri sızıntısı veya şüpheli veri transferi risklerini skorlar.
+ */
+
 public class NetworkLog extends Log{
-    private double cikanVeri;
-    private double girenVeri;
+    private final double cikanVeri;
+    private final double girenVeri;
 
     public NetworkLog(String ID, String kaynakIP, String cihazAdi, double cikanVeri, double girenVeri){
         super(ID, kaynakIP, cihazAdi);
@@ -11,12 +16,14 @@ public class NetworkLog extends Log{
     @Override
     public double riskSkoruHesapla(){
 
-        double skor = 1.0;
+        double skor = 1.0;      // Temel başlangıç skoru
 
+        // 1. Kriter: Anormal Veri Oranı Analizi
         if(cikanVeri > girenVeri * 5){
             skor += 5.5; 
         } 
 
+        // 2. Kriter: Çıkan Veri Hacmi Analizi
         if(cikanVeri >=500){
             skor += 3.0;
         } else if (cikanVeri >=100){
